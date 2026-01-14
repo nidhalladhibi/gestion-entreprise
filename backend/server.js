ef72c80d15691769e0bde00797604369af2126d5
nidhalladhibi <www.nidhal.com@gmail.com>
2026-01-14 18:38:14 +0100

    fix: remove frontend static for render

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import clientRoutes from "./routes/clientRoutes.js";
@@ -10,8 +8,10 @@ import factureRoutes from "./routes/factureRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -20,18 +20,9 @@ app.use("/api/clients", clientRoutes);
 app.use("/api/factures", factureRoutes);
 app.use("/api/products", productRoutes);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(join(__dirname, 'frontend/build')));
-  
-  app.get('(.*)', (req, res) => {
-    res.sendFile(join(__dirname, 'frontend/build', 'index.html'));
-  });
-} else {
-  app.get("/", (req, res) => res.send("API is running"));
-}
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 const PORT = process.env.PORT || 5000;
 
