dec263863e3184f3d0131d688f52072673abb18f
nidhalladhibi <www.nidhal.com@gmail.com>
2026-01-16 14:49:33 +0100

    fixed smartphone

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function Button({ children, onClick, type = "button", className = "" }) {
+export default function Button({ children, onClick, type = "button", className = "", ...rest }) {
   return (
     <button
       type={type}
       onClick={onClick}
       className={`custom-btn ${className}`}
+      {...rest}
     >
       {children}
     </button>
